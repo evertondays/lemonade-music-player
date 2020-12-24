4d487d9f717e10a0ae8da8479fa0bc6cdf485884
Everton Dias <esrdias@gmail.com>
2020-12-24 20:41:28 -0300

    Estilizando tabela de listagem de música

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { FaPlay, FaPause } from 'react-icons/fa';
 
 function Home(props){
 
@@ -7,16 +8,28 @@ function Home(props){
 			...props.music,
 			file: `http://localhost:3333/song/${props.music.file}`
 		}
+
 		props.setMedia(newMusic);
 	}
 
+	const audioTag = useRef(null);
+	const [duration, setDuration] = useState('?:??');
+
+	useEffect(() => {
+		audioTag.current.play()
+	}, []);
+
 	return(
-		<tr>
-			<th className="index-th"><button onClick={changeMusic}>Play</button></th>
+		<tr className="list-item">
+			<th className="index-th">
+				<button onClick={changeMusic} className="btn-play">< FaPlay size={15} /></button>
+			</th>
 			<th>{props.music.name}</th>
 			<th>{props.music.artist}</th>
 			<th>{props.music.album}</th>
-			<th className="duration-th">?:??</th>
+			<th className="duration-th">{duration}</th>
+
+			<audio src={props.music.file} ref={audioTag}></audio>
 		</tr>
 	);
 }
