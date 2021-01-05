e9b5734572a11b3f768835000c2a80aa4b67467c
Everton Dias <esrdias@gmail.com>
2021-01-04 22:07:03 -0300

    Resolvendo bug com o cadastro de musica

--- src/pages/RegisterMusic.js
+++ src/pages/RegisterMusic.js
@@ -1,41 +1,56 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 import '../css/form.css'
 
 function RegisterMusic(){
+	let history = useHistory();
+	const form = useRef(null);
+
+	function handleSubmit(){
+		var formData = new FormData(form.current);
+
+		axios.post('http://localhost:3333/music/', formData, {
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		})
+
+		history.push('/');
+	}
+
 	return(
 		<div className="page-container">
-			<form className="register-music" action="http://localhost:3333/music/"
-				method="POST" enctype="multipart/form-data"
-			>
+			<form ref={form} className="register-music" action="http://localhost:3333/music/">
 				<h1>Cadastre uma nova música</h1>
 
 				<div className="form-group">
 					<label>Música (arquivo):</label>
-					<input name="file" type="file" />
+					<input name="file" type="file" accept="audio/mp3" require />
 				</div>
 
 				<div className="form-group">
 					<label>Nome:</label>
-					<input name="name" type="text"/>
+					<input name="name" type="text" require />
 				</div>
 
 				<div className="form-group">
 					<label>Artista:</label>
-					<input name="artist" type="text" />
+					<input name="artist" type="text" require />
 				</div>
 
 				<div className="form-group">
 					<label>Álbum:</label>
-					<input name="album" type="text"/>
+					<input name="album" type="text" require />
 				</div>
 
 				<div className="form-group">
 					<label>Imagem (link):</label>
-					<input name="image" type="text"/>
+					<input name="image" type="text" require />
 				</div>
 
-				<button type="submit">Adicionar</button>
+				<button type="button" onClick={handleSubmit}>Adicionar</button>
 			</form>
 		</div>
 	);
